50cfd87b739a92beaabd34ff4c8ed8b409557e62
nghienthamnien <ptbinh2w@gmail.com>
2023-12-08 15:14:59 +0700

    dont know

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -2,6 +2,6 @@ const { APIError } = require('../util/common');
 const logger = require('../util/logger');
 
 module.exports = (err, req, res, next) => {
-    logger.error('Internal Server error', err, err.stack);
+    logger.error('Internal Server error', err);
     res.status(500).json(APIError('Internal server error'));
 };
